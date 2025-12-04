fa93e176d5f68446fd4f440d3312459d2651302a
Gerardo Garcia Lopez <gerardoagl96@gmail.com>
2025-12-04 14:27:54 -0500

    Simple window setup

--- src/main.cpp
+++ src/main.cpp
@@ -1,5 +1,43 @@
 #include <iostream>
 
+#define GLFW_INCLUDE_NONE
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
 int main() {
-	std::cout << "Hello World" << std::endl;
+
+	if (!glfwInit())
+	{
+		std::cout << "Something failed while initializing" << std::endl;
+		return -1;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	GLFWwindow* window = glfwCreateWindow(640, 480, "House", NULL, NULL);
+
+	if (!window)
+	{
+		std::cout << "Something failed while creating the window and its context" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+
+	glfwMakeContextCurrent(window);
+	gladLoadGL();
+
+	while (!glfwWindowShouldClose(window))
+	{
+		glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
+
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwDestroyWindow(window);
+	glfwTerminate();
 }
