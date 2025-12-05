cbe015396b6ffbb2a088fd122b73b023533b03c5
Gerardo Garcia Lopez <gerardoagl96@gmail.com>
2025-12-05 11:13:02 -0500

    House printed correctly

--- src/main.cpp
+++ src/main.cpp
@@ -40,8 +40,9 @@ int main() {
 	glEnable(GL_DEPTH_TEST);
 
 	Shader shader("res/shaders/Basic.shader");
+	Shader ceilShader("res/shaders/Ceiling.shader");
 
-	float data[] = {
+	float walls[] = {
 		 0.5f, 0.5f,  0.5f,
 		 0.5f,-0.5f,  0.5f,
 		-0.5f,-0.5f,  0.5f,
@@ -53,7 +54,17 @@ int main() {
 		-0.5f, 0.5f, -0.5f,
 	};
 
-	unsigned int index[] = {
+	float ceiling[] = {
+		 0.0f, 1.0f, 0.0f,
+
+		 0.5f, 0.5f,  0.5f,
+		-0.5f, 0.5f,  0.5f,
+
+		 0.5f, 0.5f, -0.5f,
+		-0.5f, 0.5f, -0.5f,
+	};
+
+	unsigned int wallIndex[] = {
 		0, 1, 2,
 		0, 2, 3,
 
@@ -67,8 +78,15 @@ int main() {
 		0, 4, 5
 	};
 
-	VertexBuffer vb(&data, sizeof(float) * 3 * 4 * 2);
-	IndexBuffer ib(index, 6 * 4);
+	unsigned int ceilIndex[] = {
+		0, 1, 2,
+		0, 2, 4,
+		0, 3, 4,
+		0, 1, 3
+	};
+
+	VertexBuffer vb(&walls, sizeof(float) * 3 * 4 * 2);
+	IndexBuffer ib(wallIndex, 6 * 4);
 	VertexArray va;
 	VertexBufferLayout layout;
 	Renderer renderer;
@@ -77,7 +95,18 @@ int main() {
 
 	va.addLayout(vb, layout);
 
+	vb.Unbind();
+	ib.Unbind();
+	va.Unbind();
 
+	VertexBuffer ceilVb(&ceiling, sizeof(float) * 3 * 5);
+	IndexBuffer  ceilIb(ceilIndex, 3 * 4);
+	VertexArray ceilVa;
+	VertexBufferLayout ceilLayout;
+
+	ceilLayout.Push<float>(3);
+
+	ceilVa.addLayout(ceilVb, ceilLayout);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -89,13 +118,23 @@ int main() {
 
 		model = glm::translate(model, glm::vec3(0.0f, 0.0f, 0.0f));
 		model = glm::rotate(model, (float)glfwGetTime() * glm::radians(20.0f), glm::vec3(0.0f, 1.0f, 0.0f));
-		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.0f));
+		view = glm::translate(view, glm::vec3(0.0f, 0.0f, -3.5f));
+		view = glm::rotate(view, glm::radians(20.0f), glm::vec3(1.0f, 0.0f, 0.0f));
 
+		shader.Bind();
 		shader.setUniformM4f("model", model);
 		shader.setUniformM4f("view", view);
 		shader.setUniformM4f("projection", projection);
+		shader.Unbind();
+
+		ceilShader.Bind();
+		ceilShader.setUniformM4f("model", model);
+		ceilShader.setUniformM4f("view", view);
+		ceilShader.setUniformM4f("projection", projection);
+		ceilShader.Unbind();
 
 		renderer.draw(va, ib, shader);
+		renderer.draw(ceilVa, ceilIb, ceilShader);
 
 		glfwSwapBuffers(window);
 		glfwPollEvents();
